09f005307b5b0f781e60b2ea74527c17cb47b709
Jacob Batista <jacobaccio@gmail.com>
2024-08-01 10:23:57 -0600

    Release 2.0.3

---
+++ src/main/java/design/ore/cOREAPI/datatypes/SQL/generators/ConsumerTokenRealmSigner.java
@@ -0,0 +1,27 @@
+package design.ore.cOREAPI.datatypes.SQL.generators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cORE_CONSUMER_TOKEN_REALM_SIGNER")
+public class ConsumerTokenRealmSigner
+{	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	Long id;
+	String consumerKey, consumerSecret, token, tokenSecret, realm, csvCodeIgnores;
+}
