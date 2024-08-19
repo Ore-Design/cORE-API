a0d7b1486c81699a45875942ed77cb1e9c4bd2ef
Jacob Batista <jacobaccio@gmail.com>
2024-08-19 10:17:23 -0600

    Release 2.0.4

--- src/main/java/design/ore/cOREAPI/datatypes/SQL/generators/ConsumerTokenRealmSigner.java
+++ src/main/java/design/ore/cOREAPI/datatypes/SQL/generators/ConsumerTokenRealmSigner.java
@@ -23,5 +23,5 @@ public class ConsumerTokenRealmSigner
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(nullable = false)
 	Long id;
-	String consumerKey, consumerSecret, token, tokenSecret, realm, csvCodeIgnores;
+	String consumerKey, consumerSecret, token, tokenSecret, realm;
 }
